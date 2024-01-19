c0155c8af86ed36131524ab4905d598fe9ed5a7c
Shuchita Khare <shuchitak@xmos.com>
2024-01-19 09:12:30 +0000

    Get compiling with the changed directory structure

--- app_usb_aud_xk_evk_xu316_dsp/src/dspt_main.h
+++ app_usb_aud_xk_evk_xu316_dsp/src/dspt_main.h
@@ -4,11 +4,8 @@
 #include <xcore/channel.h>
 #include <xcore/parallel.h>
 #include <stddef.h>
-#include "adsp_module.h"
-
-#include "dummy_agc.h"
-#include "dummy_parametric_eq.h"
-#include "biquad_module.h"
+#include "stages/adsp_module.h"
+#include "stages/biquad.h"
 
 DECLARE_JOB(dsp_control_thread, (chanend_t, module_instance_t **, size_t));
 DECLARE_JOB(dsp_thread, (chanend_t, chanend_t, module_instance_t**, size_t));
